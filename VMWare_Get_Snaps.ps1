e33711c38db216b8438c5daa389def0e0782b791
Boston-of-Gilead <Boston-of-Gilead@users.noreply.github.com>
2021-02-23 15:41:37 -0500

    Add files via upload

---
+++ VMWare_Get_Snaps.ps1
@@ -0,0 +1,4 @@
+﻿Connect-VIServer -Server vcenter01 -User admin -Password pass
+
+#Gets SS >3 days old
+Get-VM | Get-Snapshot | Where {$_.Created -lt (Get-Date).AddDays(-3)} | Select-Object VM, Name, Created
